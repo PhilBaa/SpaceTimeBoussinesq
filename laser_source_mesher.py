b7ae0652b7cc0aa8417f5145b9feb2cb39247d6e
PhilBaa <philipp.baasch@gmail.com>
2023-11-29 16:52:32 +0100

    added laser mesh

---
+++ laser_source_mesher.py
@@ -0,0 +1,16 @@
+import pygmsh
+
+with pygmsh.geo.Geometry() as geom:
+    geom.add_polygon(
+        [
+            [0.0, 0.0],
+            [1.0, 0.0],
+            [1.0, 1.0],
+            [0.0, 1.0],
+        ],
+        mesh_size=0.05,
+    )
+    mesh = geom.generate_mesh()
+
+
+mesh.write("out.vtk")
