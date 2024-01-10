354e51a13aaa90d0d19642dc73f43692b7289330
PhilBaa <philipp.baasch@gmail.com>
2024-01-10 16:20:59 +0100

    fixed mesher to have correct dimension

--- laser_source_mesher.py
+++ laser_source_mesher.py
@@ -1,4 +1,5 @@
 import pygmsh
+import meshio
 
 with pygmsh.geo.Geometry() as geom:
     geom.add_polygon(
@@ -13,4 +14,11 @@ with pygmsh.geo.Geometry() as geom:
     mesh = geom.generate_mesh(dim=2)
 
 
+
+
+# remove z-coordinate
+mesh = meshio.Mesh(mesh.points[:, :2], {"triangle":  mesh.get_cells_type("triangle")})
+meshio.svg.write(
+    "laser.svg", mesh, float_fmt=".3f", stroke_width="0.1"
+)
 mesh.write("laser.xml")
